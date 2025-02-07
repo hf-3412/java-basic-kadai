95ac58764e8cff2580b6bc8ed9967e012f7a081e
hf-3412 <imosaru117@gmail.com>
2025-02-07 21:16:54 +0900

    upload kadai_015 3rd

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -1,25 +1,19 @@
 package kadai_015;
 
 public class Car_Chapter15 {
-	private int gear;
-	private int speed;
-	
-	public Car_Chapter15(int gear, int speed) {
-		this.gear  = gear;
-		this.speed = speed;
-	}
+	private int gear  = 1;
+	private int speed = 10;
 
 	public void gearChange(int afterGear) {
 		System.out.println("ギア" + gear + "から" + afterGear + "に切り替えました");
 		gear = afterGear;
-		switch(gear) {
-		case 1  -> speed = 10;
-		case 2  -> speed = 20;
-		case 3  -> speed = 30; 
-		case 4  -> speed = 40;
-		case 5  -> speed = 50;
-		default -> speed = 10;
-		}
+		speed = switch(gear) {
+		case 2  -> 20;
+		case 3  -> 30; 
+		case 4  -> 40;
+		case 5  -> 50;
+		default -> 10;
+		};
 	}
 	
 	public void run() {
